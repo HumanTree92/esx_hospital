7df9345fa4f22152334907ff272629fbf5245ca2
William Plunkett Jr <joey.plunkett92@yahoo.com>
2019-11-10 06:41:49 -0600

    Add Pillbox Hill
    - Add Pillbox Hill Location

--- config.lua
+++ config.lua
@@ -13,7 +13,7 @@ Config.BlipHospital = {
 	Scale = 1.0
 }
 
-Config.Price = 250 -- Edit this to your liking.
+Config.Price = 100 -- Edit this to your liking.
 
 Config.EnableUnemployedOnly = true -- If true it will only show Blips to Unemployed Players | false shows it to Everyone.
 Config.EnableBlips = true -- If true then it will show blips | false does the Opposite.
@@ -26,6 +26,7 @@ Config.Locations = {
 	{ x = -874.79, y = -307.56, z = 38.58, heading = 350.95 },
 	{ x = -496.97, y = -336.14, z = 33.50, heading = 253.92 },
 	{ x = 298.70, y = -584.62, z = 42.26, heading = 75.49 },
+	--{ x = 338.57,  y = -586.07,  z = 27.79, heading = 226.23 }, -- Pillbox Hill
 	{ x = 1829.24, y = 3667.16, z = 33.28, heading = 214.90 },
 	{ x = -240.31, y = 6324.13, z = 31.43, heading = 221.37 }
 }
